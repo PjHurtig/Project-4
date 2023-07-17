83ea0ee9f7326f228e6a342c83eafc71080f746b
PjHurtig <>
2023-07-17 12:24:48 +0000

    add allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['project-four-pj-1355a7167125.herokuapp.com', 'localhost']
 
 
 # Application definition
